9d487d275734e7fb54eb6ae1a38a40aa328d5f6e
Gus <garciagus1992@gmail.com>
2025-08-23 15:34:53 +0200

    add clasificación no lineal

--- support_vector_machines.py
+++ support_vector_machines.py
@@ -4,6 +4,10 @@ from sklearn import datasets
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
+from sklearn.datasets import make_moons
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import SVC
 
 # cargamos el dataset iris con la longitud y ancho del pétalo para identificar la clase 2
 iris = datasets.load_iris()
@@ -91,3 +95,115 @@ plt.title("$C = {}$".format(svm_clf2.C), fontsize=16)
 plt.axis([4, 5.9, 0.8, 2.8])
 
 plt.show()
+
+
+
+# ---- Clasificación no lineal ----
+
+# creamos un dataset con make_moons
+X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
+
+# y mostramos el gráfico
+def plot_dataset(X, y, axes):
+    plt.plot(X[:, 0][y==0], X[:, 1][y==0], "bs")
+    plt.plot(X[:, 0][y==1], X[:, 1][y==1], "g^")
+    plt.axis(axes)
+    plt.grid(True, which='both')
+    plt.xlabel(r"$x_1$", fontsize=20)
+    plt.ylabel(r"$x_2$", fontsize=20, rotation=0)
+
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+plt.show()
+
+# creamos una pipeLine para entrenar el modelo con LinearSVC
+polynomial_svm_clf = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=3)),
+        ("scaler", StandardScaler()),
+        ("svm_clf", LinearSVC(C=10, loss="hinge", random_state=42))
+    ])
+
+polynomial_svm_clf.fit(X, y)
+
+
+# y mostramos el gráfico
+def plot_predictions(clf, axes):
+    x0s = np.linspace(axes[0], axes[1], 100)
+    x1s = np.linspace(axes[2], axes[3], 100)
+    x0, x1 = np.meshgrid(x0s, x1s)
+    X = np.c_[x0.ravel(), x1.ravel()]
+    y_pred = clf.predict(X).reshape(x0.shape)
+    y_decision = clf.decision_function(X).reshape(x0.shape)
+    plt.contourf(x0, x1, y_pred, cmap=plt.cm.brg, alpha=0.2)
+    plt.contourf(x0, x1, y_decision, cmap=plt.cm.brg, alpha=0.1)
+
+plot_predictions(polynomial_svm_clf, [-1.5, 2.5, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+
+plt.show()
+
+# creamos ahora la pipeLine con SVC y el parámetro de kernel
+poly_kernel_svm_clf = Pipeline([
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=5))
+    ])
+poly_kernel_svm_clf.fit(X, y)
+
+
+# mismo model que el anterior pero con un degree de 10
+poly100_kernel_svm_clf = Pipeline([
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="poly", degree=10, coef0=100, C=5))
+    ])
+poly100_kernel_svm_clf.fit(X, y)
+
+
+
+fig, axes = plt.subplots(ncols=2, figsize=(10.5, 4), sharey=True)
+
+
+# mostramos ambos gráficos
+plt.sca(axes[0])
+plot_predictions(poly_kernel_svm_clf, [-1.5, 2.45, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.4, -1, 1.5])
+plt.title(r"$d=3, r=1, C=5$", fontsize=18)
+
+plt.sca(axes[1])
+plot_predictions(poly100_kernel_svm_clf, [-1.5, 2.45, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.4, -1, 1.5])
+plt.title(r"$d=10, r=100, C=5$", fontsize=18)
+plt.ylabel("")
+
+plt.show()
+
+
+
+# mismo ejemplo pero con un kernel diferente y parámetros
+gamma1, gamma2 = 0.1, 5
+C1, C2 = 0.001, 1000
+hyperparams = (gamma1, C1), (gamma1, C2), (gamma2, C1), (gamma2, C2)
+
+svm_clfs = []
+for gamma, C in hyperparams:
+    rbf_kernel_svm_clf = Pipeline([
+            ("scaler", StandardScaler()),
+            ("svm_clf", SVC(kernel="rbf", gamma=gamma, C=C))
+        ])
+    rbf_kernel_svm_clf.fit(X, y)
+    svm_clfs.append(rbf_kernel_svm_clf)
+
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10.5, 7), sharex=True, sharey=True)
+
+for i, svm_clf in enumerate(svm_clfs):
+    plt.sca(axes[i // 2, i % 2])
+    plot_predictions(svm_clf, [-1.5, 2.45, -1, 1.5])
+    plot_dataset(X, y, [-1.5, 2.45, -1, 1.5])
+    gamma, C = hyperparams[i]
+    plt.title(r"$\gamma = {}, C = {}$".format(gamma, C), fontsize=16)
+    if i in (0, 1):
+        plt.xlabel("")
+    if i in (1, 3):
+        plt.ylabel("")
+
+plt.show()
+
+
